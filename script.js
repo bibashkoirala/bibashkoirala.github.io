0ef5d39ce57803f76aa8662b356b9bbde7d83989
BibashJr <bibashkoirala78@gmail.com>
2023-08-04 14:11:18 +0545

    detail added

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const descriptionElement = document.getElementById('description');
+  const descriptionText =
+    "A passionate web developer with a love for Django and building scalable applications. Let's connect and collaborate!";
+  let index = 0;
+
+  function type() {
+    descriptionElement.textContent += descriptionText[index];
+    index++;
+    if (index < descriptionText.length) {
+      setTimeout(type, 100); // Typing speed (adjust if needed)
+    }
+  }
+
+  setTimeout(type, 1000); // Delay before typing starts (adjust if needed)
+});
